0f086d99b546ce320f2b83ccc5920cdf538a3870
Dhiraj Gyawali <114153679+DhirozZ@users.noreply.github.com>
2024-06-04 17:18:16 +0530

    Add files via upload

---
+++ operator.js
@@ -0,0 +1,38 @@
+// Arithmetic Operators.
+
+let a = 50;
+let b = 5;
+
+console.log("Value of a is:",a);
+console.log("Value of b is:",b);
+console.log("Arithmetic Operators:")
+
+console.log("a + b =", (a + b));
+console.log("a - b =", (a - b));
+console.log("a * b =", (a * b));
+console.log("a / b =", (a / b));
+console.log("a % b =", (a % b));
+console.log("a ** b =", (a ** b));
+console.log("a++ =", (a++)); 
+console.log(a);
+console.log("b-- =", (b--));
+console.log(b);
+console.log("++a =", (++a));
+console.log("--a =", (--b));
+
+// Assignment Operators
+
+console.log("Assignment Operators:")
+
+let aa = 10;
+console.log("Value of aa is:",aa);
+let bb = 5;
+console.log("Value of bb is:",bb);
+console.log("aa += bb =", (aa += bb));
+console.log("aa -= bb =", (aa -= bb));
+console.log("aa *= bb =", (aa *= bb));
+console.log("aa /= bb =", (aa /= bb));
+console.log("aa %= bb =", (aa %= bb));
+
+
+
